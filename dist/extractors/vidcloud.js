ef5c643ef41d09cd2b01a11bbb48d523d50995b1
nedbaker12 <116677560+nedbaker12@users.noreply.github.com>
2023-07-25 17:58:16 +0530

    Update vidcloud.js

--- dist/extractors/vidcloud.js
+++ dist/extractors/vidcloud.js
@@ -23,11 +23,55 @@ class VidCloud {
                     'Referer': videoUrl.href,
                 },
             };
+
+
             const { data } = await axios_1.default.get(`${isAlternative ? this.host2 : this.host}/ajax/embed-4/getSources?id=${id}`, options);
+            // console.log(data.sources)
             let sources = null;
             if (!(0, utils_1.isJson)(data.sources)) {
-                const key = await (await axios_1.default.get('https://raw.githubusercontent.com/enimax-anime/key/e4/key.txt')).data;
-                sources = JSON.parse(crypto_js_1.default.AES.decrypt(data.sources, key).toString(crypto_js_1.default.enc.Utf8));
+                const stops = await (await axios_1.default.get('https://raw.githubusercontent.com/enimax-anime/key/e4/key.txt')).data;
+                // console.log(stops)
+                // console.log('preset')
+                // console.log(data)
+                // console.log("preset")
+                const nextgen = data.sources
+                // sources = JSON.parse(crypto_js_1.default.AES.decrypt(data.sources, key).toString(crypto_js_1.default.enc.Utf8));
+                function extractAndConcatenateString(nextgen, stops) {
+                  // Helper function to extract substrings based on the given stops
+                function extractSubstring(startIndex, endIndex) {
+                    return nextgen.slice(startIndex, endIndex);
+                  }
+
+                let decryptedKey = '';
+                let newOriginalString = nextgen; // Create a copy of the original string
+
+                  // Extract substrings from the stops and concatenate them
+                stops.forEach((stop) => {
+                    const [startIndex, endIndex] = stop;
+                    decryptedKey += extractSubstring(startIndex, endIndex);
+
+                    // Replace the extracted substring with an equal length of spaces in the new original string
+                    const spaces = ' '.repeat(endIndex - startIndex);
+                    newOriginalString = newOriginalString.slice(0, startIndex) + spaces + newOriginalString.slice(endIndex);
+                    });
+
+                  newOriginalString = newOriginalString.replace(/\s/g, '');
+                  return {
+                    decryptedKey,
+                    newOriginalString,
+                  };
+                }
+
+                // Call the function and get the result
+                const { decryptedKey, newOriginalString } = extractAndConcatenateString(nextgen, stops);
+
+                // console.log(`Concatenated extracted key (decryption key): ${decryptedKey}\n`);
+                // console.log(`New encrypted string (original without extracted parts): ${newOriginalString}\n`);
+
+                // const sources = JSON.parse(CryptoJS.AES.decrypt(newOriginalString, decryptedKey).toString(CryptoJS.enc.Utf8));
+                sources = JSON.parse(crypto_js_1.default.AES.decrypt(newOriginalString, decryptedKey).toString(crypto_js_1.default.enc.Utf8));
+
+                // console.log(`Decrypted string: `, sources);
             }
             for (const source of sources) {
                 const { data } = await axios_1.default.get(source.file, options);
@@ -45,8 +89,8 @@ class VidCloud {
             }
             videoResult.subtiles = data.tracks.map((track) => {
                 return {
-                    url: track.file,
-                    lang: track.label ?? 'Default',
+                    file: track.file,
+                    lable: track.label ?? 'Default',
                 };
             });
             return videoResult;
